7addbacfee000a5635b11a28386e2dc83e325252
Ryan Mohta <ryanmohta@gmail.com>
2022-11-03 15:51:15 -0700

    Update FAQ with new dates and info

--- src/faq.js
+++ src/faq.js
@@ -5,11 +5,11 @@ const faq = [
   },
   {
     question: "Where is WebJam being held?",
-    answer: "WebJam is being held in a hybrid environment, consisting of online events/workshops and an in-person closing ceremony. Correspondence and announcements will happen primarily through the WebJam Discord; registered participants will receive an invitation to the Discord closer to the start of WebJam."
+    answer: "WebJam is being held in an in-person environment, consisting of events/workshops and ceremonies. Correspondence and announcements will happen primarily through the WebJam Discord; registered participants will receive an invitation to the Discord closer to the start of WebJam."
   },
   {
     question: "When is WebJam?",
-    answer: "WebJam is happening between October 11th - October 15th. The kickoff event is on Monday, October 11th, 2020 at 6PM PST and the final presentations are on Friday, October 15th, 2020 at 5PM PST."
+    answer: "WebJam is happening between November 14th - November 18th. The kickoff event is on Monday, November 18th, 2022 at 4PM PST and the final presentations are on Friday, November 18th, 2022 at 7PM PST."
   },
   {
     question: "How much does it cost?",
@@ -25,7 +25,7 @@ const faq = [
   },
   {
     question: "What if I don’t have a team?",
-    answer: "We will be assisting with team formations during the event by creating a Discord channel for team formations."
+    answer: "We will be assisting with team formations during the event by creating a Discord channel for team formations. Also, we plan to have a social after our kickoff ceremony for students to find potential teammates!"
   },
   {
     question: "How do I register?",
@@ -45,7 +45,7 @@ const faq = [
   },
   {
     question: "What are the rules?",
-    answer: "Please refer to the <a href=\"https://docs.google.com/document/d/1ycr7GI5N0F07lPkZwsaUn3muEDj0RBQ2NbDSYNk79sA/edit\">WebJam Rules and Code of Conduct</a> for more information."
+    answer: "Please refer to the <a href=\"https://docs.google.com/document/d/1T2Dh_FVx0zU8xhNC2CHDMb_0xg-j2oFBUfp-M0C4FqM/edit\">WebJam Rules and Code of Conduct</a> for more information."
   },
 ];
 
